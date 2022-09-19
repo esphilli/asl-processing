eaaa1bd48e1233e7c85a5447041f2112aa2eb2d3
Eliana Phillips <52674277+esphilli@users.noreply.github.com>
2022-09-19 09:56:33 -0700

    Add files via upload

---
+++ get_oxford_asl.py
@@ -0,0 +1,134 @@
+import os 
+import sys
+import glob
+import json
+import numpy as np 
+import pydicom as dcm
+import nibabel as nib 
+
+#### MAIN METHOD PURPOSE ####
+# takes single ASL study dir after dcm2niix as input
+# assumes BIDS metadata present and fsl_anat already ran
+# reads metadata (& DICOM headers?) for ASL image in study
+# then auto-populates parameters for oxford_asl call
+
+def get_oxford_asl(study):
+	
+	# load data
+	assert os.path.isdir(study)
+	os.chdir(study)
+	jsons = glob.glob('*.json')
+	anat = glob.glob('*.anat')[0]
+	asls = []
+	for j in jsons:
+		if 'PASL' in j or '_real' in j:
+			asls.append(j)
+	# assign data as temp to get manufacturer info		
+	data = json.load(open(asls[0]))		
+
+	# parse metadata to get parameters
+	if data['Manufacturer'] == 'Siemens':
+		# make sure data is original ASL image
+		if 'ORIGINAL' in data['ImageType']:
+			filename = asls[0]
+		else:
+			for a in asls:
+				d = json.load(open(a))
+				if 'ORIGINAL' in d['ImageType']:
+					data = d
+					filename = a
+					break
+		iaf = 'ct' 
+		tis = str(data['InversionTime'])
+		bolus = str(data['BolusDuration'])
+		fslanat = anat
+		oxford_asl_call = 'oxford_asl -i ' + filename.replace('.json','.nii') + ' -o . --iaf=' + iaf + ' --tis=' + tis + ' --bolus=' + bolus + ' --fslanat=' + anat	
+		# get fieldmaps if are present and add to oxford_asl_call
+		fieldmaps = []
+		for j in jsons:
+			if 'Field_Mapping' in j:
+				fieldmaps.append(j)
+		if len(fieldmaps) != 0:
+			calculated_fieldmap, echospacing, pedir = get_fieldmap(fieldmaps)
+			oxford_asl_call = oxford_asl_call + ' --fmap=' + calculated_fieldmap + ' --echospacing=' + echospacing + ' --pedir=' + pedir
+		if data['MRAcquisitionType'] == '2D':
+			slice_times = data['SliceTiming']
+			slicedt = str(slice_times[1] - slice_times[0])
+			oxford_asl_call = oxford_asl_call + ' --slicedt=' + slicedt
+			# get M0 image from first volume of data if it exists
+			img = nib.load(filename.replace('.json','.nii'))
+			img_data = img.get_fdata()
+			if img_data.shape[3] % 2 == 1:
+				m0 = img_data[:,:,:,0]
+				m0 = nib.Nifti1Image(m0,img.affine,img.header)
+				nib.save(m0,'m0.nii')
+				tr = str(data['RepetitionTime'])
+				oxford_asl_call = oxford_asl_call + ' -c m0.nii --tr=' + tr 
+		elif data['MRAcquisitionType'] == '3D':
+			pass	
+	elif data['Manufacturer'] == 'GE':
+		# make sure data is derived ASL image and calibration is original
+		if 'DERIVED' in data['ImageType'] and 'CBF' not in data['ImageType']:
+			filename = asls[0]
+		else:
+			for a in asls:
+				d = json.load(open(a))
+				if 'DERIVED' in d['ImageType'] and 'CBF' not in d['ImageType']:	
+					data = d
+					filename = a
+					break
+		iaf = 'diff'
+		tis = str(data['LabelingDuration'] + data['PostLabelingDelay'])
+		bolus = str(data['LabelingDuration'])
+		oxford_asl_call = 'oxford_asl -i ' + filename.replace('.json','.nii') + ' -o . --casl --iaf=' + iaf + ' --tis=' + tis + ' --bolus=' + bolus + ' --fslanat=' + anat	
+		# calibration parameters
+		calib = 0
+		for a in asls:
+			d = json.load(open(a))
+			if 'ORIGINAL' in d['ImageType']:
+				calib = d
+				calib_filename = a
+				break
+		if calib:
+			tr = str(calib['RepetitionTime'])
+			oxford_asl_call = oxford_asl_call + ' -c ' + calib_filename.replace('.json','.nii') + ' --tr=' + tr
+	elif data['Manufacturer'] == 'Philips' and data['MRAcquisitionType'] == '2D':
+		pass
+		#iaf = 'tc'
+	# outputs call to oxford_asl
+	print('oxford_asl call:')
+	print(oxford_asl_call)
+	return oxford_asl_call	
+
+# prepares the fieldmap for use with Siemens PASL data
+def get_fieldmap(fieldmaps):
+	# run BET on magnitude image
+	for f in fieldmaps:
+		if '_e2' in f and '_ph' not in f:
+			os.system('bet '+f.replace('.json','.nii')+' '+f.replace('.json','_be.nii'))
+			fieldmap_mag_be = f.replace('.json','_be.nii.gz')
+		elif '_e2_ph' in f:
+			fieldmap_phase_filename = json.load(open(f))
+			fieldmap_phase = f.replace('.json','.nii')	
+	# run fsl_prepare_fieldmap
+	os.system('fsl_prepare_fieldmap SIEMENS '+fieldmap_phase+' '+fieldmap_mag_be+' calculated_fieldmap.nii 2.46')
+	# output calculated_fieldmap and fieldmap parameters
+	calculated_fieldmap = 'calculated_fieldmap.nii.gz'
+	echospacing = str(fieldmap_phase_filename['DwellTime'])
+	if fieldmap_phase_filename['PhaseEncodingDirection'] == 'i':
+		pedir = 'x'
+	elif fieldmap_phase_filename['PhaseEncodingDirection'] == 'i-':
+		pedir = '-x'
+	elif fieldmap_phase_filename['PhaseEncodingDirection'] == 'j':
+		pedir = 'y'
+	elif fieldmap_phase_filename['PhaseEncodingDirection'] == 'j-':
+		pedir = '-y'
+	elif fieldmap_phase_filename['PhaseEncodingDirection'] == 'k':
+		pedir = 'z'
+	elif fieldmap_phase_filename['PhaseEncodingDirection'] == 'k-':
+		pedir = '-z'		
+	print('prepared fieldmap for use with oxford_asl')
+	return calculated_fieldmap, echospacing, pedir
+
+if __name__=='__main__':
+	get_oxford_asl(sys.argv[1])
